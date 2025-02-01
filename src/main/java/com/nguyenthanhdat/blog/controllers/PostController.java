8221f622f11d99a01d1603b090530abc0a764db5
Nguyen Thanh Dat <nthdat09@gmail.com>
2025-02-02 00:26:11 +0700

    feat: fix get posts api

--- src/main/java/com/nguyenthanhdat/blog/controllers/PostController.java
+++ src/main/java/com/nguyenthanhdat/blog/controllers/PostController.java
@@ -1,23 +1,29 @@
 package com.nguyenthanhdat.blog.controllers;
 
+import com.nguyenthanhdat.blog.domain.dtos.post.PostDto;
 import com.nguyenthanhdat.blog.domain.entities.Post;
+import com.nguyenthanhdat.blog.mappers.PostMapper;
 import com.nguyenthanhdat.blog.services.impl.PostServiceImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Optional;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/v1/posts")
 @RequiredArgsConstructor
 public class PostController {
     private final PostServiceImpl postServiceImpl;
+    private final PostMapper postMapper;
 
     @GetMapping
-    public List<Post> getAllPosts() {
-        return postServiceImpl.getAllPosts();
+    public List<PostDto> getAllPosts() {
+        return postServiceImpl.getAllPosts()
+                .stream()
+                .map(postMapper::toDto)
+                .collect(Collectors.toList());
     }
 
     @GetMapping("/{slug}")
